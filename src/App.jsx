349e6a38afbbfc710c887c8f14325e9bf4175f6f
mchowdhury2023 <chowdhury7135@saskpolytech.ca>
2023-09-15 03:16:36 -0600

    wrote Registration component and updated APP.jsx file to manage eventhandler

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
+import Courses from './components/Courses/Courses'
+import Header from './components/Header'
+import Registration from './components/Registration/Registration'
 import React, { useState } from 'react'
 
 function App() {
+  const [selectedCourses, setSelectedCourses] = useState([]);
+  const [totalCredits, setTotalCredits] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
 
+  const handleCourseSelect = (course) => {
+    if (!selectedCourses.find(item => item.title === course.title)) {
+      if (totalCredits + course.credit <= 20) {
+        setSelectedCourses([...selectedCourses, course]);
+        setTotalCredits(prevCredits => prevCredits + course.credit);
+        setTotalPrice(prevPrice => prevPrice + course.price);
+      } else {
+        alert('Credit exceeds 20 or remaining credit goes below 0!');
+      }
+    } else {
+      alert('This course has already been added!');
+    }
+  };
 
   return (
     <>
   
       <Header></Header>
       <div className='flex mt-10'>
-      <Courses ></Courses>
-       
+        <Courses onCourseSelect={handleCourseSelect} ></Courses>
+        <Registration
+          selectedCourses={selectedCourses} 
+          totalCredits={totalCredits} 
+          totalPrice={totalPrice}
+        />
 
       </div>
    
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-
-const Registration = () => {
-    return (
-      <div className='w-1/4 ml-10'>
-     
+const Registration = ({ selectedCourses, totalCredits, totalPrice }) => {
+  return (
+    <div className='w-1/4 ml-10'>
+      <div className="bg-white p-6 border-2 border-gray-200 rounded-lg shadow-md">
+      <p className="text-lg mb-2"><strong>Remaining total hours: {20 - totalCredits}</strong></p>
+        <h1 className="text-xl font-bold mb-4">Selected Course Name:</h1>
+        <ul className="mb-4 space-y-2">
+          {selectedCourses.map(course => (
+            <li key={course.id} className="p-2 bg-gray-100 rounded border border-gray-200">{course.title}</li>
+          ))}
+        </ul>
+        <p className="text-lg mb-2"><strong>Total Credit Hour:</strong> {totalCredits}</p>
+        <p className="text-lg mb-2"><strong>Total Price:</strong> {totalPrice} USD</p>
       </div>
-    )
-  }
-  
-  export default Registration;
+    </div>
+  )
+}
+
+export default Registration;
   
   
